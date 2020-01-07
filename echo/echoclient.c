1cdb92ff09789a2da48b1c9e6aecee7bfba06f18
Tony Mason <fsgeek@gatech.edu>
2020-01-07 09:43:37 -0800

    Initial version Spring 2020 code

---
+++ echo/echoclient.c
@@ -0,0 +1,86 @@
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <getopt.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+/* A buffer large enough to contain the longest allowed string */
+#define BUFSIZE 520
+
+#define USAGE                                                                       \
+    "usage:\n"                                                                      \
+    "  echoclient [options]\n"                                                      \
+    "options:\n"                                                                    \
+    "  -s                  Server (Default: localhost)\n"                           \
+    "  -p                  Port (Default: 20502ls)\n"                                  \
+    "  -m                  Message to send to server (Default: \"Hello world.\")\n" \
+    "  -h                  Show this help message\n"
+
+/* OPTIONS DESCRIPTOR ====================================================== */
+static struct option gLongOptions[] = {
+    {"server", required_argument, NULL, 's'},
+    {"port", required_argument, NULL, 'p'},
+    {"message", required_argument, NULL, 'm'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}};
+
+/* Main ========================================================= */
+int main(int argc, char **argv)
+{
+    int option_char = 0;
+    char *hostname = "localhost";
+    unsigned short portno = 20502;
+    char *message = "Hello World!!";
+
+    // Parse and set command line arguments
+    while ((option_char = getopt_long(argc, argv, "s:p:m:hx", gLongOptions, NULL)) != -1)
+    {
+        switch (option_char)
+        {
+        case 's': // server
+            hostname = optarg;
+            break;
+        case 'p': // listen-port
+            portno = atoi(optarg);
+            break;
+        default:
+            fprintf(stderr, "%s", USAGE);
+            exit(1);
+        case 'm': // message
+            message = optarg;
+            break;
+        case 'h': // help
+            fprintf(stdout, "%s", USAGE);
+            exit(0);
+            break;
+        }
+    }
+
+    setbuf(stdout, NULL); // disable buffering
+
+    if ((portno < 1025) || (portno > 65535))
+    {
+        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
+        exit(1);
+    }
+
+    if (NULL == message)
+    {
+        fprintf(stderr, "%s @ %d: invalid message\n", __FILE__, __LINE__);
+        exit(1);
+    }
+
+    if (NULL == hostname)
+    {
+        fprintf(stderr, "%s @ %d: invalid host name\n", __FILE__, __LINE__);
+        exit(1);
+    }
+
+    /* Socket Code Here */
+
+}
